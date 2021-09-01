2277961da281008f48e06c8a8db3edeed0c95b23
Pteute <88045793+Pteute@users.noreply.github.com>
2021-09-01 11:51:12 +0200

    details de celebrite
    + creation entité médias pour afficher plsr médias à la célébrité

--- src/Entity/Celebrite.php
+++ src/Entity/Celebrite.php
@@ -49,9 +49,17 @@ class Celebrite
      */
     private $activites;
 
+    /**
+     * @ORM\OneToMany(targetEntity=Medias::class, mappedBy="celebrite")
+     */
+    private $medias;
+
+
+
     public function __construct()
     {
         $this->activites = new ArrayCollection();
+        $this->medias = new ArrayCollection();
     }
 
     public function getId(): ?int
@@ -102,7 +110,7 @@ class Celebrite
 
     public function incrementFame()
     {
-        $this->fame += 1;//$this->fame = $this->fame + 1;
+        $this->fame += 1; //$this->fame = $this->fame + 1;
     }
 
     public function setFame(?int $fame): self
@@ -144,6 +152,36 @@ class Celebrite
     public function removeActivite(Activite $activite): self
     {
         $this->activites->removeElement($activite);
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Medias[]
+     */
+    public function getMedias(): Collection
+    {
+        return $this->medias;
+    }
+
+    public function addMedia(Medias $media): self
+    {
+        if (!$this->medias->contains($media)) {
+            $this->medias[] = $media;
+            $media->setCelebrite($this);
+        }
+
+        return $this;
+    }
+
+    public function removeMedia(Medias $media): self
+    {
+        if ($this->medias->removeElement($media)) {
+            // set the owning side to null (unless already changed)
+            if ($media->getCelebrite() === $this) {
+                $media->setCelebrite(null);
+            }
+        }
 
         return $this;
     }
