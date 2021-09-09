f223bff6f5bf3b33e2c7e1572a05a974e287982f
Haaomas <barreauji@gmail.com>
2021-09-09 10:57:07 +0200

    Accueil  ++

--- assets/admin.js
+++ assets/admin.js
@@ -1,6 +1,7 @@
 // admin.js
 // import d'une feuille de style SASS pour surcharger les variables boostrap et qui importe les SCSS de bootstrap
 import './styles/admin.scss';
+import './styles/icon.css'
 
 // Import des JS du Framework HTML bootstrap
 import 'bootstrap';
